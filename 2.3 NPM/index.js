d0ecd9523df439c70f3e05b8c3fd3c376b34f821
suihua <2254579398@qq.com>
2025-02-02 22:45:47 +0800

    init

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// var generateName = require("sillyname");
+
+import generateName from "sillyName";
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`)
+
+import {randomSuperhero} from 'superheroes';
+console.log(`\n\nI am ${randomSuperhero()} !! Be Ready to Die!\n\n`)
